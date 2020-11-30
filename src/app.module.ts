b65f490ed0e5dd779718450c7569a2eafc7aebea
Gonzalo Tanoira <gonzalo.mtanoira@gmail.com>
2020-11-30 16:35:55 -0300

    Fix in the database definitions

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,41 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-// Environment
-import { MYSQL_HOST, MYSQL_PORT, MYSQL_SYNCHRONIZE } from './environment/environment.settings';
 // Modules
 import { GridModule } from './grid/grid.module';
-//Entities
-import { Batch } from './models/batch.entity';
-import { Channel } from './models/channel.entity';
-import { Grid } from './models/grid.entity';
+// Databases
+import { globosatProvider } from './database/database.providers';
 
 @Module({
   imports: [
     GridModule,
     TypeOrmModule.forRoot({
-      name: 'GLOBOSAT',
-      type: 'mysql',
-      host: `${MYSQL_HOST}`,
-      port: MYSQL_PORT,
-      username: 'root',
-      password: 'root123',
-      database: 'globosat',
-      entities: [
-        Batch,
-        Channel,
-        Grid
-      ],
-      synchronize: MYSQL_SYNCHRONIZE
-    }),
+      ...globosatProvider,
+      type: 'mysql'
+    })
   ],
   controllers: [
     AppController
   ],
   providers: [
-    AppService
+    AppService,
   ],
 })
-export class AppModule {
-  // constructor(private connection: Connection) {}
-}
+export class AppModule {}
